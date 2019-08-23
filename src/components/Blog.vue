935ced1521e6b971df15d4a0ac02247edd947585
imamichi <imamichi0419@gmail.com>
2019-08-23 21:22:32 +0900

    firebase blog実装

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,26 +1,55 @@
 <template>
   <section>
     <h1>Blog</h1>
-    <div v-html="markdown"></div>
+    <div id="blog">
+      <div v-for="(data, index) in blogData">
+        <h2>{{data.title}}</h2>
+        <div>{{data.date}}</div>
+        <div v-html="data.content"></div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import markdown from "../assets/_posts/2019-08-22-json.md";
+// import markdown from "../assets/_posts/2019-08-22-json.md";
+import firebase from 'firebase'
 
 export default {
   name: 'Blog',
+  data: function() {
+    return {
+      db: firebase.database(),
+      blogData: []
+    }
+  },
   props: {
     msg: String
   },
   computed: {
-    markdown() {
-      return markdown;
-    }
+    // markdown() {
+    //   return markdown;
+    // }
   },
   mounted() {
+    // firebaseからblogデータ取得
+    const ref = this.db.ref("flamelink/environments/production/content/newSchema/en-US");
+    let blogData = this.blogData;
 
-  },
+    ref.once("value")
+    .then(function(snapshot) {
+      const postData = snapshot.val();
+      Object.keys(postData).forEach(id => {
+        const setData = {
+          title: postData[id].title,
+          date: postData[id].date,
+          content: postData[id].content
+        }
+        blogData.push(setData);
+      });
+    });
+    
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from "firebase";
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC8xutq7TZEaAN-aP4X8NmtwiL8Txh-TFY",
+  authDomain: "jabee-works.firebaseapp.com",
+  databaseURL: "https://jabee-works.firebaseio.com",
+  projectId: "jabee-works",
+  storageBucket: "jabee-works.appspot.com",
+  messagingSenderId: "222923581837",
+  appId: "1:222923581837:web:6ecccd76699a92b2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
